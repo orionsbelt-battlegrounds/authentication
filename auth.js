276b0ad3d9b1777ddbe1ed6f94202f99c9346d11
Nuno Silva <nuno@Nunos-MBP.lan>
2014-12-21 16:16:27 +0000

    server with a default response in the auth method

--- auth.js
+++ auth.js
@@ -1,7 +1,21 @@
-(function auth(exports) {
 
-  var auth = exports;
+var restify = require('restify');  
+var mongojs = require('mongojs');
 
-  auth.package = require('./package.json');
+var server = restify.createServer({
+	name: 'orionsbeltauth',
+	version: '0.1.0'
+});
 
-})(exports);
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+server.get("/auth", function (req, res, next) {
+    res.send("auth response");
+    return next();
+});
+
+server.listen(3000, function () {
+    console.log("Server started @ 3000");
+});
